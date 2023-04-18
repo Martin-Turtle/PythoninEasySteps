a074f29f489dc4f92567840ad51efa590fde3859
Martin-Turtle <info@martinturtle.co.uk>
2023-04-18 19:31:31 +0100

    Examples

---
+++ decimal.py
@@ -0,0 +1,13 @@
+from _pydecimal import *
+
+
+
+item = Decimal( 0.70 )
+rate = Decimal( 1.05 )
+
+tax = item * rate
+total = item + tax
+
+print('Item:\t', '%.2f' % item)
+print('Tax:\t' , '%.2f'%tax)
+print('Total:\t', '%.2f'%total)
